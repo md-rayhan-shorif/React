0c9ecb7b64a7ee8112b024e1a16ea2c8a938bada
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-01-07 02:06:06 +0600

    Day 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-        
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      <Card/>  
+      
     </>
   )
 }
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = () => {
+  let counter = 5;
+
+  const addValue = () => {
+    counter = counter + 1
+
+  }
+
+
+  return (
+    <>
+      <h1 >
+    Hello world!
+  </h1>
+    </>
+  )
+}
+
+export default Card
