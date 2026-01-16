013e966075b0fab5c9e772217c9318832d57c755
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-01-16 22:43:48 +0600

    [Starting Part 2 of React Learning

--- my-app/src/projects/MiniContextProject/Components/Theme.js
+++ my-app/src/projects/MiniContextProject/Components/Theme.js
@@ -8,7 +8,7 @@ export const ThemeContext = createContext(
         darkTheme: () => {},
         lightTheme: () => {},
     }
-)
+) 
 export const ThemeProvider =  ThemeContext.Provider;
 
 export default function useTheme(){
---
+++ react-p2/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=' text-5xl font-bold text-white text-center  bg-black h-screen'>
+        Hi , I am good
+      </div>
+    </>
+  )
+}
+
+export default App
