47d76fa027a5324f30f0dda6ef4bfb31c719b864
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-01-19 14:16:56 +0600

    [Project - My Blog Website] starting

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+   console.log(import.meta.env.VITE_APPWRITE_URL )
+
+  return (
+    <>
+      <h1 className=' text-4xl text-center'>This is my Mega Project</h1>
+    </>
+  )
+}
+
+export default App
