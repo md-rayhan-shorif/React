9883659b1f18abdffa42b275684f379317c46d1f
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-01-23 00:04:20 +0600

    [Project authentication] complete

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,10 +1,24 @@
-
-
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
 
 function App() {
-   console.log(import.meta.env.VITE_APPWRITE_URL )
-
+   const [loading, setLoading] = useState(true);
+   const dispatch = useDispatch()
+  
+   useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+   },[])
   return (
     <>
       <h1 className=' text-4xl text-center'>This is my Mega Project</h1>
