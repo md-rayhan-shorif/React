14a25759b0d76eaed74c9bbc60e658407f056761
md-rayhan-shorif <rayhanshorif01@gmail.com>
2026-01-23 18:07:21 +0600

    [My To Do list] project with Redux toolkit

---
+++ To-do-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <p className=" text-4xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
